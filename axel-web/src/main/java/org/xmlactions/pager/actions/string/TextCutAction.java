4b4c635aff22d7b3f95bcad01db548eaa98df6b4
mike <mwjmurphy@gmail.com>
2020-06-12 17:16:31 +0100

    Adding text_cut action - as String.substring

---
+++ axel-web/src/main/java/org/xmlactions/pager/actions/string/TextCutAction.java
@@ -0,0 +1,111 @@
+package org.xmlactions.pager.actions.string;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmlactions.action.actions.BaseAction;
+import org.xmlactions.action.config.IExecContext;
+
+/*
+ * <axel:text_cut text="asdasd" from="0" to="10" key="akey"/>
+ */
+public class TextCutAction extends BaseAction {
+
+	private static Logger log = LoggerFactory.getLogger(TextCutAction.class);
+
+	private String text;
+	private Integer from;	// default = 0
+	private Integer to;		// default = end of text
+	private String key;
+
+
+	public String execute(IExecContext execContext)
+	{
+		String cut = "";
+		validate();
+		if (getFrom() != null && getFrom() >= getText().length()) {
+			return "";
+		}
+		if (getFrom() != null && getTo() != null && getFrom() >= getTo()) {
+			return "";
+		}
+		if (getFrom() != null) {
+			if (getTo() != null) {
+				cut = getText().substring(getFrom(), getTo());
+			} else {
+				cut = getText().substring(getFrom());
+			}
+		} else if (getTo() != null) {
+			cut = getText().substring(0, getTo());
+		}
+		
+		if (StringUtils.isNotBlank(getKey())) {
+			execContext.put(getKey(), cut);
+			cut = "";
+		}
+		return cut;
+	}
+	
+	private void validate() {
+		if (StringUtils.isBlank(getText())) {
+			throw new IllegalArgumentException("Missing text attribute for text_cut");
+		}
+		
+		if (getFrom() == null && getTo() == null) {
+			throw new IllegalArgumentException("Must set either from or the to attribure for text_cut");
+		}
+		
+		if (getFrom() != null && getFrom() < 0) {
+			setFrom(0);
+		}
+		
+		if (getTo() != null && getTo() > getText().length()) {
+			setTo(getText().length());
+		}
+		if (getTo() != null && getTo() < 0) {
+			setTo(0);
+		}
+	}
+
+	public String toString()
+	{
+		return ("text_cut(" + getText() + "," + getFrom() + "," + getTo() + ")");
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Integer getFrom() {
+		return from;
+	}
+
+	public void setFrom(Integer from) {
+		this.from = from;
+	}
+
+	public Integer getTo() {
+		return to;
+	}
+
+	public void setTo(Integer to) {
+		this.to = to;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	
+
+
+}
+
